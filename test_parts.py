fd4ad5260c1131ed56611dd8b9993b9666da4727
ChinmayDesai2005 <58367566+ChinmayDesai2005@users.noreply.github.com>
2022-06-03 20:17:18 +0530

    Add files via upload

---
+++ test_parts.py
@@ -0,0 +1,114 @@
+import unittest
+from datetime import datetime
+
+from battery.nubbin import NubbinBattery
+from battery.spindler import SpindlerBattery
+from engine.capulet import CapuletEngine
+from engine.willoughby import WilloughbyEngine
+from engine.sternman import SternmanEngine
+from tires.carrigan import CarriganTires
+from tires.octoprime import OctoprimeTires
+
+
+# Engines
+class testCapulet(unittest.TestCase):
+    def test_engine_should_be_serviced(self):
+        current_mileage = 30001
+        last_service_mileage = 0
+        engine = CapuletEngine(current_mileage, last_service_mileage)
+        self.assertTrue(engine.needs_service())
+
+
+    def test_engine_should_not_be_serviced(self):
+        current_mileage = 30000
+        last_service_mileage = 0
+        engine = CapuletEngine(current_mileage, last_service_mileage)
+        self.assertFalse(engine.needs_service())
+
+
+class testWilloughby(unittest.TestCase):
+    def test_engine_should_be_serviced(self):
+        current_mileage = 60001
+        last_service_mileage = 0
+        engine = WilloughbyEngine(current_mileage, last_service_mileage)
+        self.assertTrue(engine.needs_service())
+
+
+    def test_engine_should_not_be_serviced(self):
+        current_mileage = 60000
+        last_service_mileage = 0
+        engine = WilloughbyEngine(current_mileage, last_service_mileage)
+        self.assertFalse(engine.needs_service())
+
+
+class testSternman(unittest.TestCase):
+    def test_engine_should_be_serviced(self):
+        warning_light_is_on = True
+        engine = SternmanEngine(warning_light_is_on)
+        self.assertTrue(engine.needs_service())
+
+
+    def test_engine_should_not_be_serviced(self):
+        warning_light_is_on = False
+        engine = SternmanEngine(warning_light_is_on)
+        self.assertFalse(engine.needs_service())
+
+
+#Batteries
+class testSpindler(unittest.TestCase):
+    def test_battery_should_be_serviced(self):
+        today = datetime.today().date()
+        last_service_date = today.replace(year=today.year - 4)
+        battery = SpindlerBattery(today, last_service_date)
+        self.assertTrue(battery.needs_service())
+
+
+    def test_battery_should_not_be_serviced(self):
+        today = datetime.today().date()
+        last_service_date = today.replace(year=today.year - 2)
+        battery = SpindlerBattery(today, last_service_date)
+        self.assertFalse(battery.needs_service())
+
+
+class testNubbin(unittest.TestCase):
+    def test_battery_should_be_serviced(self):
+        today = datetime.today().date()
+        last_service_date = today.replace(year=today.year - 5)
+        battery = NubbinBattery(today, last_service_date)
+        self.assertTrue(battery.needs_service())
+
+
+    def test_battery_should_not_be_serviced(self):
+        today = datetime.today().date()
+        last_service_date = today.replace(year=today.year - 3)
+        battery = NubbinBattery(today, last_service_date)
+        self.assertFalse(battery.needs_service())
+
+
+#Tires
+class testCarrigan(unittest.TestCase):
+    def test_tires_should_be_serviced(self):
+        tire_wear = [1, 1, 1, 1]
+        tires = CarriganTires(tire_wear)
+        self.assertTrue(tires.needs_service())
+    
+    def test_tires_should_not_be_serviced(self):
+        tire_wear = [0, 0, 0, 0]
+        tires = CarriganTires(tire_wear)
+        self.assertFalse(tires.needs_service())
+
+
+class testOctoprime(unittest.TestCase):
+    def test_tires_should_be_serviced(self):
+        tire_wear = [1, 1, 1, 1]
+        tires = OctoprimeTires(tire_wear)
+        self.assertTrue(tires.needs_service())
+    
+    def test_tires_should_not_be_serviced(self):
+        tire_wear = [0, 0, 0, 0]
+        tires = OctoprimeTires(tire_wear)
+        self.assertFalse(tires.needs_service())
+
+
+if __name__ == '__main__':
+    unittest.main()
